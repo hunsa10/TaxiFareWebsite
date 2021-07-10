0e16368ad0f3c2e1d574c06763aa22cad3853372
Sarah Hunt <hunt.sarah10@gmail.com>
2021-07-10 15:48:16 +1000

    code updated

--- app.py
+++ app.py
@@ -7,28 +7,25 @@ import requests
 '''
 
 st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string).
-Or as with this paragraph using the `st.` functions
+Predict your taxi fare!
 ''')
 
-## Here we would like to add some controllers in order to ask the user to
-## select the parameters of the ride
-## 1. Let's ask for:
-## - date and time
-## - pickup longitude
-## - pickup latitude
-## - dropoff longitude
-## - dropoff latitude
-## - passenger count
+# Here we would like to add some controllers in order to ask the user to
+# select the parameters of the ride
+# 1. Let's ask for:
+# - date and time
+# - pickup longitude
+# - pickup latitude
+# - dropoff longitude
+# - dropoff latitude
+# - passenger count
 
 
 # Pickup datetime
-pickup_date = st.date_input('What date and time is your trip?',
+pickup_date = st.date_input('What date is your trip?',
                             datetime.date(2021, 1, 1))
 
-pickup_time = st.time_input('What date and time is your trip?',
+pickup_time = st.time_input('What time is your trip?',
                             datetime.time(9, 0, 0))
 
 pickup_datetime = f'{pickup_date} {pickup_time}'
@@ -48,8 +45,8 @@ passenger_count = st.number_input('Passenger_count', 1)
 
 
 # Once we have these, let's call our API in order to retrieve a prediction
-# See ? No need to load a `model.joblib` file in this app, we do not even need to
-# know anything about Data Science in order to retrieve a prediction...
+# See ? No need to load a `model.joblib` file in this app, we do not even need
+# to know anything about Data Science in order to retrieve a prediction...
 # 🤔 How could we call our API ? Off course... The `requests` package 💡
 
 
