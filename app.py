226298e864c9f3be2c2f39b9be51cd3a964630e6
Sarah Hunt <hunt.sarah10@gmail.com>
2021-08-08 22:21:16 +1000

    Updated README and app comments

--- app.py
+++ app.py
@@ -4,21 +4,13 @@ import requests
 
 '''
 # TaxiFareModel
+Creates a simple streamlit website to predict taxi fare from pickup and dropoff
+input. (Prediction model code from TaxiFareModel)
 '''
 
 st.markdown('''
 Predict your taxi fare!
 ''')
-
-# Here we would like to add some controllers in order to ask the user to
-# select the parameters of the ride
-# 1. Let's ask for:
-# - date and time
-# - pickup longitude
-# - pickup latitude
-# - dropoff longitude
-# - dropoff latitude
-# - passenger count
 
 
 # Pickup datetime
@@ -44,11 +36,7 @@ dropoff_latitude = st.number_input('Dropoff longitude', 1)
 passenger_count = st.number_input('Passenger_count', 1)
 
 
-# Once we have these, let's call our API in order to retrieve a prediction
-# See ? No need to load a `model.joblib` file in this app, we do not even need
-# to know anything about Data Science in order to retrieve a prediction...
-# 🤔 How could we call our API ? Off course... The `requests` package 💡
-
+# Call API to retrieve the price prediction
 
 url = 'https://taxifare.lewagon.ai/predict'
 
@@ -65,8 +53,3 @@ response = requests.get(url, params)
 prediction = response.json()['prediction']
 
 st.write(f'Your fare is: ${round(prediction, 2)}')
-
-# 2. Let's build a dictionary containing the parameters for our API...
-# 3. Let's call our API using the `requests` package...
-# 4. Let's retrieve the prediction from the **JSON** returned by the API...
-# Finally, we can display the prediction to the user
